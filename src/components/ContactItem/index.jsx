2250f132a0cc22f0071396572ceba66c2210fe29
Serhii-Matskevych <sergeyma1992@gmail.com>
2022-10-23 12:33:36 +0300

    1

--- src/components/ContactItem/index.jsx
+++ src/components/ContactItem/index.jsx
@@ -18,7 +18,7 @@ export function ContactItem({ contact, item, onDelete }) {
   );
 }
 ContactItem.propTypes = {
-   contact: PropTypes.string.isRequired,
-  item: PropTypes.number.isRequired,
-  onDelete: PropTypes.func.isRequired
+   name: PropTypes.string.isRequired,
+  number: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired
 };
